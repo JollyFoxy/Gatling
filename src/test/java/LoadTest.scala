36ac1aa1b48c213f42e0c71ac368737e9f349022
pavel <pavel.mizirev@gmail.com>
2023-01-31 15:36:54 +0300

    Заработало!!! 2

--- src/test/java/LoadTest.scala
+++ src/test/java/LoadTest.scala
@@ -1,3 +1,14 @@
-class LoadTest {
+import Request._
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
+class LoadTest extends Simulation{
+  val httpConf=http.baseUrl("https://demoqa.com/swagger/")
+
+  val snc = scenario("First").exec(account)
+
+  setUp(snc.inject(constantUsersPerSec(100)during(15 seconds))
+    .protocols(httpConf))
 }
--- src/test/java/Request.scala
+++ src/test/java/Request.scala
@@ -1,3 +1,8 @@
-object Request {
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
 
+object Request {
+  val account =http("account")
+    .get("/Account/v1/User")
+    .check(status.is(200))
 }
