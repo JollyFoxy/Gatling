8fc1704dff8e5f3cc26fe739832361b9a6123c55
pavel <pavel.mizirev@gmail.com>
2023-01-31 17:28:11 +0300

    Заработало!!! 4

--- src/test/java/Request.scala
+++ src/test/java/Request.scala
@@ -1,10 +1,27 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import java.util.UUID
+
 object Request {
-  val account =exec(
-    http("account")
-    .get("/Account/v1/User")
+  val account =
+  exec(
+    http("post")
+    .post("/Account/v1/Authorized")
     .check(status.is(200))
+  )
+  val getUser = exec(
+    session => session.set("UUID", UUID.randomUUID().toString)
+  ).exec(
+    http("get")
+      .get("/Account/v1/User/${UUID}")
+      .check(status.is(200))
+  )
+  val deleteUser = exec(
+    session => session.set("UUID", UUID.randomUUID().toString)
+  ).exec(
+    http("delete")
+      .delete("/Account/v1/User/${UUID}")
+      .check(status.is(200))
   )
 }
