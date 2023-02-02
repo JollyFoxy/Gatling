6554d6113c6d2e7817679aa03c2cd52ac9f49101
pavel <pavel.mizirev@gmail.com>
2023-02-02 17:24:16 +0300

    Заработало!!! 6

--- src/test/java/LoadTest.scala
+++ src/test/java/LoadTest.scala
@@ -1,13 +1,15 @@
 import Request._
 import io.gatling.http.Predef._
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 class LoadTest extends Simulation{
-  val httpConf=http.baseUrl("https://demoqa.com/swagger/#/")
+  val httpConf: HttpProtocolBuilder =http.baseUrl("https://demoqa.com/swagger/#/")
 
-  val snc = scenario("First")randomSwitch(
+  val snc: ScenarioBuilder = scenario("First")randomSwitch(
     (25,account),
     (25,getUser),
     (25,deleteUser),
@@ -15,6 +17,16 @@ class LoadTest extends Simulation{
   )
 
   setUp(
-    snc.inject(constantUsersPerSec(100)during(25 minute))
+    snc.inject(
+      rampConcurrentUsers(10).to(1000).during(20 seconds)
+      /*nothingFor(4), // 1
+      atOnceUsers(10), // 2
+      rampUsers(10).during(5), // 3
+      constantUsersPerSec(20).during(15), // 4
+      constantUsersPerSec(20).during(15).randomized, // 5
+      rampUsersPerSec(10).to(20).during(10.minutes), // 6
+      rampUsersPerSec(10).to(20).during(10.minutes).randomized, // 7
+      stressPeakUsers(1000).during(20)*/
+    )
     .protocols(httpConf))
 }
--- src/test/java/Request.scala
+++ src/test/java/Request.scala
@@ -1,30 +1,31 @@
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 
 import java.util.UUID
 
 object Request {
-  val account =
+  val account: ChainBuilder =
   exec(
     http("post")
     .post("/Account/v1/Authorized")
     .check(status.is(200))
   )
-  val getUser = exec(
+  val getUser: ChainBuilder = exec(
     session => session.set("UUID", UUID.randomUUID().toString)
   ).exec(
     http("get")
       .get("/Account/v1/User/${UUID}")
       .check(status.is(200))
   )
-  val deleteUser = exec(
+  val deleteUser: ChainBuilder = exec(
     session => session.set("UUID", UUID.randomUUID().toString)
   ).exec(
     http("delete")
       .delete("/Account/v1/User/{UUID}")
       .check(status.is(200))
   )
-  val postUser = exec(
+  val postUser: ChainBuilder = exec(
     session => session.set("userName", UUID.randomUUID().toString)
   ).exec(
     session => session.set("password", UUID.randomUUID().toString)
